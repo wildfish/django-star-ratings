520221a2dc2f2228885528d09833d9784dc22695
Rolo <rolo@wildfish.com>
2015-09-04 15:17:10 +0100

    Preparing setup.py for first PyPI release.

--- setup.py
+++ setup.py
@@ -1,33 +1,39 @@
 import os
 from setuptools import setup, find_packages
 
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="star-ratings",
-    version="0.1.0",
-    author="Wildfish",
-    author_email="rolo@wildfish.com",
-    description=("Django ratings"),
-    license="BSD",
-    keywords="ratings",
-    url="https://github.com/wildfish/star-ratings",
+    name='django-star-ratings',
+    version='0.1.0',
     packages=find_packages(),
-    long_description=read('README.md'),
     include_package_data=True,
+    license='BSD License',
+    description=('A Django app to add star ratings to models.'),
+    long_description=README,
+    url='https://github.com/wildfish/star-ratings',
+    author='Wildfish',
+    author_email='developers@wildfish.com',
+    keywords='ratings',
     install_requires=[
-        "Django >= 1.6, <= 1.8.4",
-        "django-model-utils",
-        "django-braces"
+        'Django >= 1.6, <= 1.8.4',
+        'django-model-utils',
+        'django-braces'
     ],
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: BSD License"
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
