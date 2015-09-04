b2eacf1adb4d482c36a7ef2270d2de889f3a3494
Dan Bate <dan@wildfish.com>
2015-09-04 11:59:47 +0100

    added tests for AggregateRating.to_dict

---
+++ tests/test_aggregate_rating_to_dict.py
@@ -0,0 +1,27 @@
+from random import random, randint
+from django.test import TestCase
+from model_mommy import mommy
+from wildfish_ratings.models import AggregateRating
+
+
+class AggregateRatingToDict(TestCase):
+    def test_fields_are_present_and_correct(self):
+        max_value = randint(0, 10)
+        total = random() * max_value
+        count = randint(1, 100)
+        avg = total / count
+
+        rating = mommy.make(
+            AggregateRating,
+            max_value=max_value,
+            rating_count=count,
+            rating_total=total,
+            rating_average=avg,
+        )
+
+        self.assertEqual(dict(
+            max_value=max_value,
+            rating_count=count,
+            rating_total=total,
+            rating_average=avg
+        ), rating.to_dict())
