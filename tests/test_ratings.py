4efc53e9080f1b09a61f18b2c5011988efef3871
Rolo <rolo@wildfish.com>
2015-09-10 12:26:15 +0100

    Updated test_ratings tests to use new AggregateRating manager method signature.

--- tests/test_ratings.py
+++ tests/test_ratings.py
@@ -26,14 +26,12 @@ class RatingsTest(TestCase):
         """
         Two different users rating the same model
         """
-        ratings = AggregateRating.objects.ratings_for_model(self.foo)
-
-        ratings = AggregateRating.objects.rate(ratings, 4, self.user_a, '127.0.0.1')
+        ratings = AggregateRating.objects.rate(self.foo, 4, self.user_a, '127.0.0.1')
         self.assertEqual(ratings.count, 1)
         self.assertEqual(ratings.total, 4)
         self.assertEqual(ratings.average, 4)
 
-        ratings = AggregateRating.objects.rate(ratings, 3, self.user_b, '127.0.0.2')
+        ratings = AggregateRating.objects.rate(self.foo, 3, self.user_b, '127.0.0.2')
         self.assertEqual(ratings.count, 2)
         self.assertEqual(ratings.total, 7)
         self.assertEqual(ratings.average, 3.5)
@@ -42,13 +40,12 @@ class RatingsTest(TestCase):
         """
         Same user rating a model twice
         """
-        ratings = AggregateRating.objects.ratings_for_model(self.foo)
-        ratings = AggregateRating.objects.rate(ratings, 4, self.user_a, '127.0.0.1')
+        ratings = AggregateRating.objects.rate(self.foo, 4, self.user_a, '127.0.0.1')
         self.assertEqual(ratings.count, 1)
         self.assertEqual(ratings.total, 4)
         self.assertEqual(ratings.average, 4)
 
-        ratings = AggregateRating.objects.rate(ratings, 2, self.user_a, '127.0.0.1')
+        ratings = AggregateRating.objects.rate(self.foo, 2, self.user_a, '127.0.0.1')
         self.assertEqual(ratings.count, 1)
         self.assertEqual(ratings.total, 2)
         self.assertEqual(ratings.average, 2)
@@ -58,10 +55,9 @@ class RatingsTest(TestCase):
         """
         If re-rating is disabled the rating should not count
         """
-        ratings = AggregateRating.objects.ratings_for_model(self.foo)
-        ratings = AggregateRating.objects.rate(ratings, 4, self.user_a, '127.0.0.1')
+        ratings = AggregateRating.objects.rate(self.foo, 4, self.user_a, '127.0.0.1')
         with self.assertRaises(ValidationError):
-            ratings = AggregateRating.objects.rate(ratings, 2, self.user_a, '127.0.0.1')
+            ratings = AggregateRating.objects.rate(self.foo, 2, self.user_a, '127.0.0.1')
 
         self.assertEqual(ratings.count, 1)
         self.assertEqual(ratings.total, 4)
@@ -70,16 +66,14 @@ class RatingsTest(TestCase):
     def test_order_by_average_rating(self):
         foo_a = self.foo = Foo.objects.create(bar='foo a')
         foo_b = self.foo = Foo.objects.create(bar='foo b')
-        rating_a = AggregateRating.objects.ratings_for_model(foo_a)
-        rating_b = AggregateRating.objects.ratings_for_model(foo_b)
 
         # Avg. rating: 3.5
-        AggregateRating.objects.rate(rating_a, 4, self.user_a, '127.0.0.1')
-        AggregateRating.objects.rate(rating_b, 3, self.user_b, '127.0.0.1')
+        AggregateRating.objects.rate(foo_a, 4, self.user_a, '127.0.0.1')
+        AggregateRating.objects.rate(foo_b, 3, self.user_b, '127.0.0.1')
 
         # Avg. rating: 1.5
-        AggregateRating.objects.rate(rating_a, 1, self.user_a, '127.0.0.1')
-        AggregateRating.objects.rate(rating_b, 2, self.user_b, '127.0.0.1')
+        AggregateRating.objects.rate(foo_a, 1, self.user_a, '127.0.0.1')
+        AggregateRating.objects.rate(foo_b, 2, self.user_b, '127.0.0.1')
 
         foos = Foo.objects.filter(ratings__isnull=False).order_by('ratings__average')
         self.assertEqual(foos[0].pk, foo_a.pk)
@@ -100,8 +94,7 @@ class RatingStr(TestCase):
         user = mommy.make(get_user_model())
         foo = mommy.make(Foo)
 
-        ratings = AggregateRating.objects.ratings_for_model(foo)
-        AggregateRating.objects.rate(ratings, 1, user, '0.0.0.0')
+        ratings = AggregateRating.objects.rate(foo, 1, user, '0.0.0.0')
         rating = ratings.ratings.get(user=user)
 
         self.assertEqual('User {} rating for {}'.format(user.pk, ratings), str(rating))
