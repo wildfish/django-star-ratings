6dffbefe43190dcaa91bfaebb158548e4ac8e0ac
Dan Bate <dan@wildfish.com>
2015-09-10 18:08:10 +0100

    added missing strategies file

---
+++ tests/strategies.py
@@ -0,0 +1,6 @@
+from hypothesis.strategies import integers
+from star_ratings import app_settings
+
+
+def scores(max_rating=app_settings.STAR_RATINGS_RANGE):
+    return integers(min_value=0, max_value=max_rating)
