6dff42f70ef83e2dad6bfe9695cbb85075f50fc5
Rolo <rolo@wildfish.com>
2015-09-11 16:26:10 +0100

    Added some setup.py niceties pinched from django-rest-framework.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 import os
+import re
+import shutil
+import sys
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
@@ -6,6 +9,35 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search('__version__ = [\'"]([^\'"]+)[\'"]', init_py).group(1)
+
+
+version = get_version('star_ratings')
+
+if sys.argv[-1] == 'publish':
+    if os.system('pip freeze | grep wheel'):
+        print('wheel not installed.\nUse `pip install wheel`.\nExiting.')
+        sys.exit()
+    if os.system('pip freeze | grep twine'):
+        print('twine not installed.\nUse `pip install twine`.\nExiting.')
+        sys.exit()
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    print('You probably want to also tag the version now:')
+    print('  git tag -a {} -m \'version {}\''.format(version, version))
+    print('  git push --tags')
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('django_star_ratings.egg-info')
+    sys.exit()
+
 
 setup(
     name='django-star-ratings',
@@ -25,6 +57,7 @@ setup(
         'django-braces'
     ],
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License'
