e1de2e7eeb5d7833eff5907e8479339f29e35437
Rolo <rolo@wildfish.com>
2015-09-14 13:09:36 +0100

    Moved rating calculation from save method to a signal, and hooked up post_delete.

---
+++ star_ratings/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+from django.db.models.signals import post_save, post_delete
+from .models import Rating
+from .signals import calculate_ratings
+
+
+class StarRatingsAppConfig(AppConfig):
+    name = 'star_ratings'
+
+    def ready(self):
+        post_save.connect(calculate_ratings, sender=Rating)
+        post_delete.connect(calculate_ratings, sender=Rating)
