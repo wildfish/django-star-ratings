7f8cfe7c8168f24172a2a35dd0921e7569d07665
Dan Bate <dan@wildfish.com>
2015-10-19 08:49:31 +0100

    added test for selecting a star

---
+++ demo/selenium_tests/driver.py
@@ -0,0 +1,72 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import os
+import sys
+from selenium import webdriver
+
+_use_remote_driver = os.environ.get('DJANGO_CONFIGURATION', '') == 'CI'
+_sauce_username = os.environ.get('SAUCE_USERNAME', '')
+_sauce_access_key = os.environ.get('SAUCE_ACCESS_KEY', '')
+
+_remote_browsers = [
+    {
+        "platform": "Mac OS X 10.9",
+        "browserName": "chrome",
+        "version": "31"
+    },
+    {
+        "platform": "Windows 8.1",
+        "browserName": "internet explorer",
+        "version": "11"
+    }
+]
+
+
+class SeleniumTestCaseMeta(type):
+    def __new__(meta, classname, bases, class_dict):
+        new_class_dict = {}
+
+        for attr_name, attr_value in class_dict.items():
+            if callable(attr_value) and attr_name.startswith('test_'):
+                method = attr_value
+
+                if _use_remote_driver:
+                    sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
+                    for i, browser in enumerate(_remote_browsers):
+                        driver_fn = lambda: webdriver.Remote(
+                            desired_capabilities=browser,
+                            command_executor=sauce_url % (_sauce_username, _sauce_access_key)
+                        )
+
+                        browser_tag = '{}_{}_{}'.format(
+                            browser['platform'],
+                            browser['browserName'],
+                            browser['version']
+                        ).replace(' ', '_')
+
+                        method_name = '{orig_name}___{tag}'.format(orig_name=attr_name, tag=browser_tag)
+                        new_class_dict[method_name] = meta._new_method(method, driver_fn)
+
+                else:
+                    driver_fn = webdriver.Chrome
+                    method_name = '{orig_name}___firefox'.format(orig_name=attr_name)
+                    new_class_dict[method_name] = meta._new_method(method, driver_fn)
+
+            else:
+                new_class_dict[attr_name] = attr_value
+
+        return type.__new__(meta, classname, bases, new_class_dict)
+
+    @classmethod
+    def _new_method(cls, method, driver_fn):
+        def _new(self):
+            driver = driver_fn()
+            try:
+                method(self, driver)
+            finally:
+                driver.quit()
+
+        return _new
+
+
+class SeleniumTestCase(StaticLiveServerTestCase, metaclass=SeleniumTestCaseMeta):
+    pass
---
+++ demo/selenium_tests/test_rate.py
@@ -0,0 +1,36 @@
+from time import sleep
+from django.contrib.auth import get_user_model
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+
+from .driver import SeleniumTestCase
+
+
+class RateTest(SeleniumTestCase):
+    def login(self, wait, username, password):
+        wait.until(EC.presence_of_element_located((By.ID, 'login-link'))).click()
+        wait.until(EC.presence_of_element_located((By.ID, 'id_username'))).send_keys(username)
+        wait.until(EC.presence_of_element_located((By.ID, 'id_password'))).send_keys(password)
+        wait.until(EC.presence_of_element_located((By.ID, 'id_submit'))).click()
+
+    def test_click_first_star___rating_is_set_to_one(self, driver):
+        get_user_model().objects.create_user('user', password='pass')
+
+        driver.get(self.live_server_url)
+
+        wait = WebDriverWait(driver, 30)
+        self.login(wait, 'user', 'pass')
+
+        wait.until(EC.presence_of_element_located((By.XPATH, '//*[@data-score="1"]'))).click()
+
+        foreground = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'star-ratings-rating-foreground')))
+        average_elem = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@class="star-ratings-rating-average"]/*[@class="star-ratings-rating-value"]')))
+        count_elem = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@class="star-ratings-rating-count"]/*[@class="star-ratings-rating-value"]')))
+        user_elem = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@class="star-ratings-rating-user"]/*[@class="star-ratings-rating-value"]')))
+
+        self.assertEqual('width: 20%;', foreground.get_attribute('style'))
+        self.assertEqual('1.00', average_elem.text)
+        self.assertEqual('1', count_elem.text)
+        self.assertEqual('1', user_elem.text)
