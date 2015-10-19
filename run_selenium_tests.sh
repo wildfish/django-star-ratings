c0a1ce47b46bf8764941bce0c38710af84971849
Dan Bate <dan@wildfish.com>
2015-10-19 16:35:10 +0100

    added some debug

--- run_selenium_tests.sh
+++ run_selenium_tests.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR=`dirname "${BASH_SOURCE[0]}"`
 
+echo $TRAVIS_PULL_REQUEST
+
 if [ -n "$TRAVIS_PULL_REQUEST" ] || [ "$1" == "-f" ]; then
     cd $SCRIPT_DIR/demo;
     python manage.py test;
