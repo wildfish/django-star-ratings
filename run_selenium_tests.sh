a69d3298b42966d180a4fc9d522e76fb79f8dfd7
Dan Bate <dan@wildfish.com>
2015-10-19 16:51:56 +0100

    fixed issue with selenium tests always being ran

--- run_selenium_tests.sh
+++ run_selenium_tests.sh
@@ -2,9 +2,8 @@
 
 SCRIPT_DIR=`dirname "${BASH_SOURCE[0]}"`
 
-echo $TRAVIS_PULL_REQUEST
 
-if [ -n "$TRAVIS_PULL_REQUEST" ] || [ "$1" == "-f" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == true ] || [ "$1" == "-f" ]; then
     cd $SCRIPT_DIR/demo;
     python manage.py test;
 fi
