a23efd949061f48d42ec03b4a9cb5dcfc33023f8
Dan Bate <dan@wildfish.com>
2015-10-26 19:23:40 +0000

    swapped drivers to only run one from the environment

--- demo/selenium_tests/runner.py
+++ demo/selenium_tests/runner.py
@@ -1,188 +1,151 @@
-from copy import copy
-from unittest import TextTestResult, TestSuite
+from unittest import TextTestResult
 from unittest.runner import _WritelnDecorator
+import sys
 from django.test.runner import DiscoverRunner
 import os
 from selenium import webdriver
-import sys
 
 _sauce_username = os.environ.get('SAUCE_USERNAME', None)
 _sauce_access_key = os.environ.get('SAUCE_ACCESS_KEY', None)
 _travis_job_number = os.environ.get('TRAVIS_JOB_NUMBER', None)
-_use_remote_driver = _travis_job_number is not None
+_browser_tag = os.environ.get('BROWSER_TAG', None)
+_use_remote_driver = _browser_tag is not None
 
-_remote_browsers = [
-    {
+_remote_browsers = {
+    'chrome_latest': {
         'platform': 'Linux',
         'browserName': 'chrome',
         'version': '',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'firefox_latest': {
         'platform': 'Linux',
         'browserName': 'firefox',
         'version': '',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'opera_latest': {
         'platform': 'Linux',
         'browserName': 'opera',
         'version': '',
-        'tunnelIdentifier': _travis_job_number,
     },
-    #
-    # disabled while ms work on the driver
-    #
-    # {
-    #     'platform': 'Windows 10',
-    #     'browserName': 'microsoftedge',
-    #     'version': '',
-    #     'tunnelIdentifier': _travis_job_number,
-    # },
-    #
-    # disabled while ms work on the driver
-    #
-    {
+    'edge_latest': {
+        'platform': 'Windows 10',
+        'browserName': 'microsoftedge',
+        'version': '',
+    },
+    'ie_11': {
         'platform': 'Windows 10',
         'browserName': 'internet explorer',
         'version': '11',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'ie_10': {
         'platform': 'Windows 8',
         'browserName': 'internet explorer',
         'version': '10',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'ie_9': {
         'platform': 'Windows 7',
         'browserName': 'internet explorer',
         'version': '9',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'safari_latest': {
         'platform': 'Mac OS X 10.11',
         'browserName': 'safari',
         'version': '',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'android_5.1': {
         'platform': 'Linux',
         'browserName': 'android',
-        'deviceName': "Android Emulator",
-        'version': "5.1",
-        'tunnelIdentifier': _travis_job_number,
+        'deviceName': 'Android Emulator',
+        'version': '5.1',
     },
-    {
+    'android_5.0': {
         'platform': 'Linux',
         'browserName': 'android',
-        'deviceName': "Android Emulator",
-        'version': "5.0",
-        'tunnelIdentifier': _travis_job_number,
+        'deviceName': 'Android Emulator',
+        'version': '5.0',
     },
-    {
+    'android_4.4': {
         'platform': 'Linux',
         'browserName': 'android',
-        'deviceName': "Android Emulator",
-        'version': "4.4",
-        'tunnelIdentifier': _travis_job_number,
+        'deviceName': 'Android Emulator',
+        'version': '4.4',
     },
-    {
+    'android_4.3': {
         'platform': 'Linux',
         'browserName': 'android',
-        'deviceName': "Android Emulator",
-        'version': "4.3",
-        'tunnelIdentifier': _travis_job_number,
+        'deviceName': 'Android Emulator',
+        'version': '4.3',
     },
-    {
+    'android_4.2': {
         'platform': 'Linux',
         'browserName': 'android',
-        'deviceName': "Android Emulator",
-        'version': "4.2",
-        'tunnelIdentifier': _travis_job_number,
+        'deviceName': 'Android Emulator',
+        'version': '4.2',
     },
-    {
+    'android_4.1':{
         'platform': 'Linux',
         'browserName': 'android',
-        'deviceName': "Android Emulator",
-        'version': "4.1",
-        'tunnelIdentifier': _travis_job_number,
+        'deviceName': 'Android Emulator',
+        'version': '4.1',
     },
-    {
+    'iphone_latest': {
         'platform': 'OS X 10.10',
         'browserName': 'iPhone',
         'deviceName': 'iPhone Simulator',
         'version': '',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'iphone_8.4': {
         'platform': 'OS X 10.10',
         'browserName': 'iPhone',
         'deviceName': 'iPhone Simulator',
         'version': '8.4',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'ipad_latest': {
         'platform': 'OS X 10.10',
         'browserName': 'iPhone',
         'deviceName': 'iPad Simulator',
         'version': '',
-        'tunnelIdentifier': _travis_job_number,
     },
-    {
+    'ipad_8.4': {
         'platform': 'OS X 10.10',
         'browserName': 'iPhone',
         'deviceName': 'iPad Simulator',
         'version': '8.4',
-        'tunnelIdentifier': _travis_job_number,
     }
-]
+}
 
 
 class SeleniumTestRunner(DiscoverRunner):
-    def _drivers(self):
-        if _use_remote_driver:
-            sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (_sauce_username, _sauce_access_key)
-            # If we are using remote browsers loop over each specified browser and wrap the test in its driver
-            for browser in _remote_browsers:
-                browser_tag = '{}_{}_{}'.format(
-                    browser['platform'],
-                    browser['browserName'],
-                    browser['version'] or 'latest',
-                ).replace(' ', '_')
+    selenium_implicit_wait = 30
 
-                yield browser_tag, webdriver.Remote(
-                    desired_capabilities=browser,
-                    command_executor=sauce_url
-                )
+    def _driver(self):
+        if _use_remote_driver:
+            sauce_url = 'http://%s:%s@ondemand.saucelabs.com:80/wd/hub' % (_sauce_username, _sauce_access_key)
+            browser = _remote_browsers[_browser_tag]
+            browser['tunnelIdentifier'] = _travis_job_number
+
+            driver = webdriver.Remote(
+                desired_capabilities=browser,
+                command_executor=sauce_url
+            )
         else:
-            # If we are not using remote drivers wrap each test with the firefox driver
-            yield 'firefox', webdriver.Firefox()
+            driver = webdriver.Firefox()
+
+        driver.implicitly_wait(self.selenium_implicit_wait)
+        return driver
 
     def run_suite(self, suite, **kwargs):
-        selenium_implicit_wait = 30
-        orig_tests = list(suite._tests)
-
         result = TextTestResult(_WritelnDecorator(sys.stderr), True, self.verbosity)
-        for tag, driver in self._drivers():
-            driver.implicitly_wait(selenium_implicit_wait)
-            try:
-                tests = []
+        driver = self._driver()
+        try:
+            # Modify each test so that they contain the correct browser
+            for t in suite._tests:
+                t.driver = driver
+                t.selenium_implicit_wait = self.selenium_implicit_wait
 
-                # Modify each test so that the description tells us which browser we are using
-                for t in orig_tests:
-                    new_test = copy(t)
-                    new_test.driver = driver
-                    new_test.selenium_implicit_wait = selenium_implicit_wait
-                    new_test._testMethodDoc = 'Ran on "{}"'.format(tag)
-                    tests.append(new_test)
-
-                TestSuite(tests).run(result)
-            finally:
-                driver.quit()
-
-                # This resets the previous class in the results so that the setupClass will be ran for the next driver
-                setattr(result, '_previousTestClass', None)
+            suite.run(result)
+        finally:
+            driver.quit()
 
         result.printErrors()
         return result
