6b19dadaa5e1594c74f0fbc21001ab66f1be12f8
Dan Bate <dan@wildfish.com>
2015-10-29 08:49:27 +0000

    no longer force running selenium tests on travis

--- scripts/ci_runtests.sh
+++ scripts/ci_runtests.sh
@@ -5,5 +5,5 @@ dirname="$(dirname "$0")"
 if [ "$RUN_WEB_TESTS" = "false" ]; then
     . $dirname/runwithcoverage.sh;
 else
-    . $dirname/runseleniumtests.sh -f;
+    . $dirname/runseleniumtests.sh;
 fi
