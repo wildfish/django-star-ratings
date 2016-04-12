daa0bed5e6d6c6c6cecf6ff8768f8b54e1e4321e
Dan Bate <dan@wildfish.com>
2016-04-12 10:38:34 +0100

    disabled selenium tests for pull requests

--- scripts/ci_runtests.sh
+++ scripts/ci_runtests.sh
@@ -2,7 +2,7 @@
 
 dirname="$(dirname "$0")"
 
-if [ "$RUN_WEB_TESTS" = "false" ]; then
+if [ "$RUN_WEB_TESTS" = "false" ] || [ ! "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     . $dirname/runwithcoverage.sh;
 else
     . $dirname/runseleniumtests.sh;
