a6041cb5f4faeff035b29b8ddf6cddba4a5742d5
Dan Bate <dan@wildfish.com>
2016-07-15 12:23:52 +0100

    made ci always run tests with coverage

--- scripts/ci_runtests.sh
+++ scripts/ci_runtests.sh
@@ -2,8 +2,5 @@
 
 dirname="$(dirname "$0")"
 
-if [ "$RUN_WEB_TESTS" = "false" ] || [ ! "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    . $dirname/runwithcoverage.sh;
-else
-    . $dirname/runseleniumtests.sh;
-fi
+. $dirname/runwithcoverage.sh;
+. $dirname/runseleniumtests.sh;
