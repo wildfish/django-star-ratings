96df91ce2ffb8e84eecf5d462c695f5c0658f819
Dan Bate <dan@wildfish.com>
2016-07-15 12:36:36 +0100

    changed run selenium logic to be either when force is set or both run selenium is set and build isnt pr

--- scripts/runseleniumtests.sh
+++ scripts/runseleniumtests.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=`dirname "${BASH_SOURCE[0]}"`
 
-if [ "$1" == "-f" ] || [ "$RUN_WEB_TESTS" != "false" ] || [ ! "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+if [ "$1" == "-f" ] || [[ "$RUN_WEB_TESTS" != "false"  &&  ! "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     cd $SCRIPT_DIR/../demo;
     python manage.py test;
 fi
