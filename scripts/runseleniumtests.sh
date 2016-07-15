1cd2891f07e10be433b636baa8dfe47cc51b552c
Dan Bate <dan@wildfish.com>
2016-07-15 12:29:50 +0100

    moved running selenium tests logic to 'runseleniumtests.sh'

--- scripts/runseleniumtests.sh
+++ scripts/runseleniumtests.sh
@@ -2,8 +2,7 @@
 
 SCRIPT_DIR=`dirname "${BASH_SOURCE[0]}"`
 
-
-if [ "$1" == "-f" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$1" == "-f" ] || [ "$RUN_WEB_TESTS" != "false" ] || [ ! "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     cd $SCRIPT_DIR/../demo;
     python manage.py test;
 fi
