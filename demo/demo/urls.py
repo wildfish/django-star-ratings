a75c494daf03be970f5146ad74d2dcd9ff328e14
Dan Bate <dan@wildfish.com>
2016-08-10 12:08:32 +0100

    swapped demo urls to not use 'patterns'

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import urls as auth_urls
 from .app.views import FooView, SizesView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', FooView.as_view(template_name='home.html'), name='home'),
     url(r'^sizes$', SizesView.as_view(), name='sizes'),
     url(r'^', include(auth_urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
-)
+]
