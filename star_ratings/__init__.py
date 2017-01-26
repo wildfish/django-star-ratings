56848413aafc93b029eff71c39a59eff0b590321
Rolo <rolo@wildfish.com>
2017-01-26 12:30:29 +0000

    Bumped version ready for release.

--- star_ratings/__init__.py
+++ star_ratings/__init__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from .app_settings import Settings
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 
 default_app_config = 'star_ratings.apps.StarRatingsAppConfig'
 app_settings = Settings()
