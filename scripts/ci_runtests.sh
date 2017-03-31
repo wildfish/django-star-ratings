6582c5d4e8191deb3fc0fb08efeeb9d0464fe857
Dan Bate <dan@wildfish.com>
2017-03-31 09:56:57 +0100

    added test checking tag version vs package version

--- scripts/ci_runtests.sh
+++ scripts/ci_runtests.sh
@@ -4,5 +4,14 @@ set -e
 
 dirname="$(dirname "$0")"
 
+if [ -n "${TRAVIS_TAG}" ]; then
+    PACKAGE_VERSION=`grep -oP '[0-9]+\.[0-9]+\.[0-9]+' star_ratings/__init__.py`
+
+    if [ "${TRAVIS_TAG}" != "${PACKAGE_VERSION}" ]; then
+        echo "Tag version (${TRAVIS_TAG}) is not equal to the package tag (${PACKAGE_VERSION})"
+        exit 1
+    fi
+fi
+
 . $dirname/runwithcoverage.sh;
 . $dirname/runseleniumtests.sh;
