710c0f150dc09e5c57a262cb8dd8c2cca118200a
Dan Bate <dan@wildfish.com>
2017-12-11 12:26:47 +0000

    removed hyp from selenium tests due to interaction with LiveServerTestCase

--- scripts/runseleniumtests.sh
+++ scripts/runseleniumtests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=`dirname "${BASH_SOURCE[0]}"`
+set -e
+
+SCRIPT_DIR=$(realpath `dirname "${BASH_SOURCE[0]}"`)
 
 if [ "$1" == "-f" ] || [[ "$RUN_WEB_TESTS" != "false"  &&  ! "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     cd $SCRIPT_DIR/../demo;
