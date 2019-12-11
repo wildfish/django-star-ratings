ee1a317d147fbd5fc56ef5c53f13dc775db74c85
Rolo <rolo@wildfish.com>
2019-12-11 15:57:17 +0000

    Added updated Python 3 version classifiers.

--- setup.py
+++ setup.py
@@ -77,5 +77,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
